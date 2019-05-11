6907929b5ec2f9cd55a778ed2b59c92a8178462b
haidt <haidtimeo@gmail.com>
2019-05-11 14:58:12 +0700

    export matcher model

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const MarketModel = require('./Model/market');
-const OrderModel = require('./Model/order');
-const Constant = require('./constant');
-module.exports = {MarketModel, OrderModel, Constant}
+const MarketModel = require("./Model/market");
+const OrderModel = require("./Model/order");
+const Constant = require("./constant");
+const MatcherModel = require("./Model/matcher");
+module.exports = { MarketModel, OrderModel, Constant, MatcherModel };
