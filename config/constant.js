9e846db69ea0719ca8bfa15c88d3bcb552bfefce
haidt <haidtimeo@gmail.com>
2019-06-12 13:47:01 +0700

    add chart config

--- config/constant.js
+++ config/constant.js
@@ -1,32 +1,36 @@
 const keys = {
+  // order
   INSERT_ORDER: "insertOrder",
   FILL_ORDER: "fillOrder",
   CANCEL_ORDER: "cancelOrder",
   REMOVE_ORDER: "removeOrder",
   UPDATE_ORDER_STATUS: "updateOrderStatus",
   INSERT_MATCHER: "insertMatcher",
-  TX_STATUS_UPDATE: "txStatusUpdate"
+  // transaction
+  TX_STATUS_UPDATE: "txStatusUpdate",
+  // chart
+  CHART_UPDATE: "updateChart"
 };
 
 const types = {
   ORDER: "order",
-  TRANSACTION: "transaction"
+  TRANSACTION: "transaction",
+  CHART: "chart"
 };
 
 const orderQueue = "orderQueue";
 const replyQueue = "replyQueue";
 
 const orderStatus = {
-  OPEN : 0,
-  FILLED : 1,
-  CANCEL : 2,
-  EXPIRED : 3,
-  WARNING : 4,
-  PENDING :5,
-  ERROR : 6,
+  OPEN: 0,
+  FILLED: 1,
+  CANCEL: 2,
+  EXPIRED: 3,
+  WARNING: 4,
+  PENDING: 5,
+  ERROR: 6,
   CURRENT_FILLING: 7
-
-}
+};
 module.exports = {
   keys,
   types,
