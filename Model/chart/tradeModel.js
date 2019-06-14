9685d0f02b2150202f936c8ed0beeb32eb11aea7
Kiet <>
2019-06-14 15:34:00 +0700

    no message

--- Model/chart/tradeModel.js
+++ Model/chart/tradeModel.js
@@ -2,10 +2,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Model = new Schema({
-  pair_id: {type: String, index: 1},
+  pair_id: {type: Number,required: true, index: 1},
   amount: {type: Number, required: true},
   price: {type: Number, required: true},
   timestamp: {type: Number, required: true, index: 1},
+  pair_symbol: {type: String, index: 1},
 
 }, {strict: false});
 module.exports = mongoose.model('trade', Model, 'trade');
