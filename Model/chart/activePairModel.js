615358243083ac4f51bcc6bb1e1e597ad5f450fe
haidt <haidtimeo@gmail.com>
2019-06-26 13:58:39 +0700

    add status of activePair model

--- Model/chart/activePairModel.js
+++ Model/chart/activePairModel.js
@@ -5,7 +5,8 @@ const Model = new Schema(
     pair_id: { type: Number, required: true, index: 1 },
     pair_name: { type: String, required: true },
     token_symbol: { type: String },
-    token_address : {type : String, required : true}
+    token_address: { type: String, required: true },
+    status: { type: String, required: true, default: 0 }
   },
   { strict: false }
 );
