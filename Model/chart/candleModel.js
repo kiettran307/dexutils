b8f85b5a7f85e1dedc34d9f731a5b6436b1b50c2
Kiet <>
2019-09-10 13:51:53 +0700

    update Candle Model

--- Model/chart/candleModel.js
+++ Model/chart/candleModel.js
@@ -2,24 +2,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Model = new Schema({
-  dateTime: {type: Date, required: true},
-  date: {type: Number, required: true, index: -1},
-  open: {type: Number, required: true},
-  close: {type: Number, required: true},
-  high: {type: Number, required: true},
-  volume: {type: Number, required: true},
-  low: {type: Number, required: true},
-  pair_id: {type: Number, required: true, index: true},
-  interval: {type: Number, required: true, index: 1},
-  resolution: {type: String, required: true},
-}, {strict: false});
-Model.pre('save', function(next) {
-  const now = new Date();
-  if (!this.createdAt) {
-    this.createdAt = now;
-  }
-  next();
-});
+  pair_id: { type: Number, index: 1, required: true },
+  date: { type: Number, required: true, index: -1 },
+  candle: { type: Array, required: true },
+  interval: { type: Number, required: true, index: 1 }
+},
+{ versionKey: false });
+
 Model.index({date: -1, pair_id: 1, interval: 1}, {unique: true});
 
 module.exports = mongoose.model('candle', Model, 'candle');
